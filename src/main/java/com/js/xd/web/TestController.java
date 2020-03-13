6383dc3fa64fe5a16f3b717a8ca51cf2473b68a7
especial <fanjun@douples.com>
2020-03-13 11:09:12 +0800

    添加swagger

--- src/main/java/com/js/xd/web/TestController.java
+++ src/main/java/com/js/xd/web/TestController.java
@@ -5,22 +5,29 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.js.xd.mapper.UserMapper;
 import com.js.xd.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 
-@Controller("/")
+@RestController
+@Api(value="swagger 测试接口", description = "swagger 测试接口")
 public class TestController {
 
     @Autowired
     private UserMapper userMapper;
 
-    @RequestMapping("bbb")
+    @PostMapping("bbb")
     @ResponseBody
+    @ApiOperation("数据库测试接口")
     public Object getTest(){
 //        List<String> aa = new ArrayList<String>();
 //        aa.add("aaa");
@@ -34,6 +41,15 @@ public class TestController {
         userWrapper.eq("userid",1);
         //使用继承了mybatis plus提供的BaseMapper  就可以使用父类中的方法进行crud
         List<User> user = userMapper.selectList(userWrapper);
-        return user;
+//        return user;
+        return userMapper.aaa();
+    }
+
+    @PostMapping("selectCars")
+    @ApiOperation("数据接受，返回测试接口")
+    public Object selectCars(@RequestBody Map<String,Object> params){
+        Map<String,Object> result = params;
+        params.put("test","testData");
+        return result;
     }
 }
